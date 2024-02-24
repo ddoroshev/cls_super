ba94180f4cb06dd32be83f66b90efd5999f1d2b8
Dima Doroshev <github@doroshev.com>
2024-02-24 14:07:07 +0200

    refactor

--- cls_super.py
+++ cls_super.py
@@ -6,28 +6,31 @@ class Super(type):
         supered_attrs = attrs.get("_supered_attrs")
         if not supered_attrs:
             for parent in base:
-                supered_attrs = getattr(parent, "_supered_attrs")
+                supered_attrs = getattr(parent, "_supered_attrs", None)
                 if supered_attrs:
                     break
+
         if not supered_attrs:
             return super().__new__(cls, name, base, attrs)
 
         for attr_name in supered_attrs:
-            if SUPER in attrs.get(attr_name, []):
-                values = attrs.pop(attr_name)
-                if isinstance(values, (list, tuple)):
-                    index = values.index(SUPER)
-                    for parent in base:
-                        parent_values = getattr(parent, attr_name, None)
-                        if not parent_values:
-                            continue
-                        values = values[:index] + parent_values + values[index + 1 :]
-                elif isinstance(values, dict):
-                    values.pop(SUPER)
-                    for parent in base:
-                        parent_values = getattr(parent, attr_name, None)
-                        if not parent_values:
-                            continue
-                        values |= parent_values
-                attrs[attr_name] = values
+            if SUPER not in attrs.get(attr_name, []):
+                continue
+            values = attrs.pop(attr_name)
+            if isinstance(values, (list, tuple)):
+                index = values.index(SUPER)
+                for parent in base:
+                    parent_values = getattr(parent, attr_name, None)
+                    if not parent_values:
+                        continue
+                    values = values[:index] + parent_values + values[index + 1 :]
+            elif isinstance(values, dict):
+                values.pop(SUPER)
+                for parent in base:
+                    values |= getattr(parent, attr_name, {})
+            elif isinstance(values, set):
+                values.remove(SUPER)
+                for parent in base:
+                    values |= getattr(parent, attr_name, set())
+            attrs[attr_name] = values
         return super().__new__(cls, name, base, attrs)
--- main.py
+++ main.py
@@ -4,31 +4,24 @@ from cls_super import Super, SUPER
 class BaseSuper(metaclass=Super):
     _supered_attrs = [
         "list_features",
+        "tuple_features",
         "dict_features",
+        "set_features",
     ]
 
 
 class Bar(BaseSuper):
-    list_features = [
-        1,
-        2,
-        3,
-    ]
-
-    dict_features = {
-        "a": "b",
-        "c": "d",
-    }
+    list_features = [1, 2, 3]
+    tuple_features = (1, 2, 3)
+    set_features = {1, 2, 3}
+    dict_features = {"a": "b", "c": "d"}
 
 
 class Baz(Bar):
-    list_features = [
-        4,
-        5,
-    ]
-    dict_features = {
-        "e": "f",
-    }
+    list_features = [4, 5]
+    tuple_features = (4, 5)
+    set_features = {4, 5}
+    dict_features = {"e": "f"}
 
 
 class Eggs(Bar):
@@ -37,6 +30,16 @@ class Eggs(Bar):
         SUPER,
         5,
     ]
+    tuple_features = (
+        4,
+        SUPER,
+        5,
+    )
+    set_features = {
+        4,
+        SUPER,
+        5,
+    }
     dict_features = {
         SUPER: "",
         "e": "f",
@@ -48,13 +51,34 @@ class Spam(Eggs):
     dict_features = {SUPER: ""}
 
 
+class BaseSuperFields(metaclass=Super):
+    _supered_attrs = ["fields"]
+
+
+class User:
+    fields = ["first_name", "last_name"]
+
+
+class UserWithId(User, BaseSuperFields):
+    fields = ["id", SUPER]
+
+
 def run():
     assert Baz.list_features == [4, 5], Baz.list_features
+    assert Baz.tuple_features == (4, 5), Baz.tuple_features
+    assert Baz.set_features == {4, 5}, Baz.set_features
     assert Baz.dict_features == {"e": "f"}, Baz.dict_features
     assert Eggs.list_features == [4, 1, 2, 3, 5], Eggs.list_features
+    assert Eggs.tuple_features == (4, 1, 2, 3, 5), Eggs.tuple_features
+    assert Eggs.set_features == {4, 1, 2, 3, 5}, Eggs.set_features
     assert Eggs.dict_features == {"a": "b", "c": "d", "e": "f"}, Eggs.dict_features
     assert Spam.list_features == [4, 1, 2, 3, 5], Spam.list_features
+    assert Spam.tuple_features == (4, 1, 2, 3, 5), Spam.tuple_features
+    assert Spam.set_features == {4, 1, 2, 3, 5}, Spam.set_features
     assert Spam.dict_features == {"a": "b", "c": "d", "e": "f"}, Spam.dict_features
+
+    assert User.fields == ["first_name", "last_name"], User.fields
+    assert UserWithId.fields == ["id", "first_name", "last_name"], UserWithId.fields
 
 
 if __name__ == "__main__":
