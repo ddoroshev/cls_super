46bd3acf6b3c23cdd71f4b43803a46eb5de02695
Dima Doroshev <github@doroshev.com>
2024-02-24 12:45:58 +0200

    Add support for dicts

--- main.py
+++ main.py
@@ -15,51 +15,78 @@ class Super(type):
         for attr_name in supered_attrs:
             if SUPER in attrs.get(attr_name, []):
                 values = attrs.pop(attr_name)
-                index = values.index(SUPER)
-                for parent in base:
-                    parent_values = getattr(parent, attr_name, None)
-                    if not parent_values:
-                        continue
-                    values = values[:index] + parent_values + values[index + 1 :]
+                if isinstance(values, (list, tuple)):
+                    index = values.index(SUPER)
+                    for parent in base:
+                        parent_values = getattr(parent, attr_name, None)
+                        if not parent_values:
+                            continue
+                        values = values[:index] + parent_values + values[index + 1 :]
+                elif isinstance(values, dict):
+                    values.pop(SUPER)
+                    for parent in base:
+                        parent_values = getattr(parent, attr_name, None)
+                        if not parent_values:
+                            continue
+                        values |= parent_values
                 attrs[attr_name] = values
         return super().__new__(cls, name, base, attrs)
 
 
 class BaseSuper(metaclass=Super):
-    _supered_attrs = ["features", "foo"]
+    _supered_attrs = [
+        "list_features",
+        "dict_features",
+    ]
 
 
 class Bar(BaseSuper):
-    features = [
+    list_features = [
         1,
         2,
         3,
     ]
 
+    dict_features = {
+        "a": "b",
+        "c": "d",
+    }
+
 
 class Baz(Bar):
-    features = [
+    list_features = [
         4,
         5,
     ]
+    dict_features = {
+        "e": "f",
+    }
 
 
 class Eggs(Bar):
-    features = [
+    list_features = [
         4,
         SUPER,
         5,
     ]
+    dict_features = {
+        SUPER: "",
+        "e": "f",
+    }
 
 
 class Spam(Eggs):
-    features = [SUPER]
+    list_features = [SUPER]
+    dict_features = {SUPER: ""}
 
 
 def run():
-    assert Baz.features == [4, 5], Baz.features
-    assert Eggs.features == [4, 1, 2, 3, 5], Eggs.features
-    assert Spam.features == [4, 1, 2, 3, 5], Spam.features
+    assert Baz.list_features == [4, 5], Baz.list_features
+    assert Baz.dict_features == {"e": "f"}, Baz.dict_features
+    assert Eggs.list_features == [4, 1, 2, 3, 5], Eggs.list_features
+    assert Eggs.dict_features == {"a": "b", "c": "d", "e": "f"}, Eggs.dict_features
+    assert Spam.list_features == [4, 1, 2, 3, 5], Spam.list_features
+    assert Spam.dict_features == {"a": "b", "c": "d", "e": "f"}, Spam.dict_features
 
 
 if __name__ == "__main__":
