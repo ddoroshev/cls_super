3d46704c107592de8d701a5149fa37ec232a537d
Dima Doroshev <github@doroshev.com>
2024-02-24 12:33:11 +0200

    Add _supered_attrs

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 SUPER = object()
 
 
-class Foo(type):
+class Super(type):
     def __new__(cls, name, base, attrs):
-        features = attrs.pop("features", [])
-        if SUPER in features:
-            index = features.index(SUPER)
-            for klass in base:
-                klass_features = getattr(klass, "features", None)
-                if not klass_features:
-                    continue
-                features = features[:index] + klass_features + features[index + 1 :]
-        return super().__new__(cls, name, base, {**attrs, "features": features})
+        supered_attrs = attrs.get('_supered_attrs')
+        if not supered_attrs:
+            for parent in base:
+                supered_attrs = getattr(parent, '_supered_attrs')
+                if supered_attrs:
+                    break
+        if not supered_attrs:
+            return super().__new__(cls, name, base, attrs)
+
+        for attr_name in supered_attrs:
+            if SUPER in attrs.get(attr_name, []):
+                values = attrs.pop(attr_name)
+                index = values.index(SUPER)
+                for parent in base:
+                    parent_values = getattr(parent, attr_name, None)
+                    if not parent_values:
+                        continue
+                    values = values[:index] + parent_values + values[index + 1 :]
+                attrs[attr_name] = values
+        return super().__new__(cls, name, base, attrs)
 
 
-class Bar(metaclass=Foo):
+class BaseSuper(metaclass=Super):
+    _supered_attrs = ['features', 'foo']
+
+
+class Bar(BaseSuper):
     features = [
         1,
         2,
@@ -42,9 +57,9 @@ class Spam(Eggs):
 
 
 def run():
-    assert Baz.features == [4, 5], baz.features
-    assert Eggs.features == [4, 1, 2, 3, 5], eggs.features
-    assert Spam.features == [4, 1, 2, 3, 5], spam.features
+    assert Baz.features == [4, 5], Baz.features
+    assert Eggs.features == [4, 1, 2, 3, 5], Eggs.features
+    assert Spam.features == [4, 1, 2, 3, 5], Spam.features
 
 
 if __name__ == "__main__":
